c7953c15e8a4a5ed7ddde04cdbb3c76be5f518ba
Jean Haro <JeanCHL98@hotmail.com>
2022-10-11 20:05:17 -0500

    Validar campos vacios

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,9 @@
 
 const { Router } = require('express');
 
+// Validación de campos
+const { check } = require('express-validator');
+
 // Controlador
 const { getAdmin, crearAdmin } = require('../controllers/admin');
 
@@ -13,6 +16,9 @@ const router = Router();
 // Obtener admin
 router.get('/', getAdmin);
 // Crear admin
-router.post('/', crearAdmin)
+router.post('/', [
+    check('email', 'El correo electrónico es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty()
+], crearAdmin)
 
 module.exports = router;
