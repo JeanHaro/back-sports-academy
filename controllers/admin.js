ee9a13a70f74da9b213498c18aa3989b8878fe52
Jean Haro <JeanCHL98@hotmail.com>
2022-10-11 19:51:25 -0500

    Validar correo único del administrador

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,20 +12,42 @@ const getAdmin = async (request, response) => {
     })
 }
 
+// Crear administradores
 const crearAdmin = async (request, response) => {
     // Valores del body
     const { email, password } = request.body;
 
-    // Instanciamos la estructura del Admin
-    const admin = new Admin(request.body);
+    // Verificar si el admin ya existe
+    try {
+        // Encontrar los email de los admin
+        const existeEmail = await Admin.findOne({ email });
 
-    // Guardar en la base de datos
-    await admin.save();
+        // Si el email ya existe
+        if (existeEmail) {
+            return response.status(400).json({
+                ok: false,
+                msg: 'El correo ya está registrado'
+            })
+        }
 
-    response.json({
-        ok: true,
-        admin
-    })
+        // Instanciamos la estructura del Admin
+        const admin = new Admin(request.body);
+
+        // Guardar en la base de datos
+        await admin.save();
+
+        response.json({
+            ok: true,
+            admin
+        })
+    } catch (error) {
+        console.log(error);
+        // 500 - error interno
+        response.status(500).json({
+            ok: false,
+            msg: 'Error inesperado... revisar registros'
+        })
+    }
 }
 
 module.exports = {
